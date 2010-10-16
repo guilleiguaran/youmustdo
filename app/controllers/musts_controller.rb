8f1fe4f909de779415d76a0539e95559b368df44
Mario Tatis <mariotatis@Tatis.local>
2010-10-15 21:30:15 -0500

    must

--- app/controllers/musts_controller.rb
+++ app/controllers/musts_controller.rb
@@ -1,5 +1,9 @@
 class MustsController < ApplicationController
 
+  def index
+    @musts = current_user.musts
+  end
+  
   def new
     @user = current_user
     @must = Must.new(params[:must])
@@ -10,7 +14,7 @@ class MustsController < ApplicationController
     @must.user = current_user
     if @must.save
       flash[:notice] = "Awesome, one more thing they must do!"
-      render :action => "new"
+      render :action => "index"
     else
       flash[:error] = "Ouch sorry, Something's not right down there, please verify your information and try again."
       render :action => "new"
--- app/models/must.rb
+++ app/models/must.rb
@@ -1,3 +1,4 @@
 class Must < ActiveRecord::Base
     belongs_to :user
+    validates_presence_of :name
 end
