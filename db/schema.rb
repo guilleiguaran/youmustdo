fb8b18eb0c9885f55a91fc28bfcc9380596770cf
guilleiguaran <guilleiguaran@gmail.com>
2010-10-16 08:58:25 -0500

    Add URL to Musts

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016062642) do
+ActiveRecord::Schema.define(:version => 20101016135048) do
 
   create_table "agrees", :force => true do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20101016062642) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "users", :force => true do |t|
