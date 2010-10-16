534a9c2e71db0bc481d54cd2246c60f39e889b1e
Alvaro Insignares <alvaro@koombea.com>
2010-10-16 13:12:44 -0500

    adding custom routes to the categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find(params[:id])
-    @musts = @category.musts
+    @category = Category.find_by_name(params[:category])
+    if @category.nil?
+      render(:file => "#{RAILS_ROOT}/public/404.html", :head => 404)
+    else 
+      @musts = @category.musts
+    end
   end
 end
