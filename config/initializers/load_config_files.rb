ceb71f6a5490d7f7362c727f7016d1758d6ddaa7
Alvaro Insignares <alvaro@koombea.com>
2010-10-15 20:27:57 -0500

    add key files to the initializars

---
+++ config/initializers/load_config_files.rb
@@ -0,0 +1,3 @@
+TWITTER_AUTH = YAML.load_file("#{RAILS_ROOT}/config/twitter.yml")[RAILS_ENV]
+FACEBOOK = YAML.load_file("#{RAILS_ROOT}/config/facebook.yml")[RAILS_ENV]
+ZENCODER = YAML.load_file("#{RAILS_ROOT}/config/zencoder.yml")[RAILS_ENV]
