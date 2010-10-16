06dd5df7dee72463001c828ea2770f35553201ce
gealgaro <german.garcia.d@gmail.com>
2010-10-16 02:14:37 -0500

    Integrated facebook connect

--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,16 @@ Rails::Initializer.run do |config|
 
   config.gem "clearance", :version => '0.8.8'
   config.gem "paperclip", :version => '2.3.4'
-  config.gem "aws-s3", :version => '0.6.2', :lib => "aws/s3"
-  config.gem "jrails", :version => '0.6.0'
-  config.gem "twitter",                   :version => '>= 0.9.8'
-  config.gem "oauth",                     :version => '>= 0.4.1'
+  config.gem "aws-s3",    :version => '0.6.2', :lib => "aws/s3"
+  config.gem "jrails",    :version => '0.6.0'
+  config.gem "twitter",   :version => '>= 0.9.8'
+  config.gem "oauth",     :version => '>= 0.4.1'
+  # dependencies for oauth2
+  #config.gem "addressable",     :version => '>= 2.2.2'
+  #config.gem "multipart-post",  :version => '>= 1.0.1'
+  config.gem "rack",            :version => '>= 1.2.1'
+  config.gem "faraday",         :version => '>= 0.5.1'
+  config.gem "oauth2",    :version => '>= 0.1.0'
 
   config.time_zone = 'UTC'
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,20 @@ ActionController::Routing::Routes.draw do |map|
   
 
   
-  # Twitter Connect
+  # Routes for social networks
+  # ==================================================================================================
+  # Facebook
+  map.facebook_auth     '/facebook/auth',       :controller => 'facebook/sessions', :action => 'new'
+  map.facebook_connect  '/facebook/connect',    :controller => 'facebook/sessions', :action => 'create'
+  map.namespace :facebook do |facebook|
+    facebook.resource :users, :only => [:new, :create]
+  end
+  # Twitter
   map.twitter_login   '/twitter/login',     :controller => 'twitter/sessions',  :action => 'create'
   map.twitter_session '/twitter/connect',   :controller => 'twitter/sessions',  :action => 'connect'
   map.namespace :twitter do |twitter|
     twitter.resource :users, :only => [:new, :create]
   end
-  
+  # ==================================================================================================
 
 end
