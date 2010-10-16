763c4ab7942e54cd484ef2ea6ce8a79d2172a4ee
Mario Tatis <mariotatis@Tatis.local>
2010-10-15 20:47:48 -0500

    adding must

--- app/controllers/musts_controller.rb
+++ app/controllers/musts_controller.rb
@@ -1,7 +1,19 @@
 class MustsController < ApplicationController
-  
+
   def new
     @user = current_user
     @must = Must.new(params[:project])
+  end
+
+  def create
+    @must = Project.new(params[:project])
+    @must.user = current_user
+    if @must.save
+      flash[:notice] = "Awesome, one more thing they must do!"
+      render :action => "new"
+    else
+      flash[:error] = "Ouch sorry, Something's not right down there, please verify your information and try again."
+      render :action => "new"
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def submit_or_cancel(form, name='Cancel')
+    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  end
+  
 end
