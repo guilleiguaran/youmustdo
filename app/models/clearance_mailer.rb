f2aaeeeb66fcaa94f4ea3cf56a3c27403bd1e023
guilleiguaran <guilleiguaran@gmail.com>
2010-10-16 00:41:34 -0500

    Adding PostageApp mailer

---
+++ app/models/clearance_mailer.rb
@@ -0,0 +1,23 @@
+require "postageapp/mailer"
+
+class ClearanceMailer < PostageApp::Mailer
+
+  def change_password(user)
+    from       Clearance.configuration.mailer_sender
+    recipients user.email
+    subject    I18n.t(:change_password,
+                      :scope   => [:clearance, :models, :clearance_mailer],
+                      :default => "Change your password")
+    body       :user => user
+  end
+
+  def confirmation(user)
+    from       Clearance.configuration.mailer_sender
+    recipients user.email
+    subject    I18n.t(:confirmation,
+                      :scope   => [:clearance, :models, :clearance_mailer],
+                      :default => "Account confirmation")
+    body      :user => user
+  end
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,9 @@ Rails::Initializer.run do |config|
   config.gem "paperclip", :version => '2.3.4'
   config.gem "aws-s3", :version => '0.6.2', :lib => "aws/s3"
   config.gem "jrails", :version => '0.6.0'
-  config.gem "twitter",                   :version => '>= 0.9.8'
-  config.gem "oauth",                     :version => '>= 0.4.1'
+  config.gem "twitter", :version => '>= 0.9.8'
+  config.gem "oauth", :version => '>= 0.4.1'
+  config.gem "postageapp", :version => '1.0.8'
 
   config.time_zone = 'UTC'
 
