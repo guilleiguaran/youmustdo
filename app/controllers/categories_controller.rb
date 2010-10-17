b312ce25ca344f230cc3e21abf00bc5ffb06c765
Mario Tatis <mariotatis@Tatis.local>
2010-10-17 17:30:11 -0500

    laksjdlkajsd

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,11 @@ class CategoriesController < ApplicationController
     if @category.nil?
       render(:file => "#{RAILS_ROOT}/public/404.html", :head => 404)
     else 
-      @musts = @category.musts.find(:all, :order => "created_at DESC", :limit => 10)
+      # @musts = @category.musts.find(:all, :order => "created_at DESC", :limit => 10)
+      
+          @musts = @category.musts.paginate :page => params[:page], :per_page => 20, :order => 'created_at DESC'
+
+
     end
   end
 end
