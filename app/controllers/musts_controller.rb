1d98760df6f5c9c0dfdb00ec3cb882a54745de5f
Mario Tatis <mariotatis@Tatis.local>
2010-10-17 17:44:01 -0500

    234

--- app/controllers/musts_controller.rb
+++ app/controllers/musts_controller.rb
@@ -19,8 +19,7 @@ class MustsController < ApplicationController
   
   def my_musts
     # @musts = current_user.musts.find(:all, :order => "created_at DESC", :limit => 10)
-        @musts = current_user.musts.paginate :page => params[:page], :per_page => 3, :order => 'created_at DESC'
-
+    @musts = current_user.musts.paginate :page => params[:page], :per_page => 20, :order => 'created_at DESC'
   end
 
   def play
@@ -28,7 +27,9 @@ class MustsController < ApplicationController
   
   def user_musts
     @user = User.find_by_username(params[:username])
-    @musts = @user.musts.find(:all, :order => "created_at DESC", :limit => 10)
+    # @musts = @user.musts.find(:all, :order => "created_at DESC", :limit => 10)
+    @musts = @user.musts.paginate :page => params[:page], :per_page => 3, :order => 'created_at DESC'
+
   end
 
   def new
