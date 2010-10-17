312e715d56de5212ca2663287e860b5beae97ff9
gealgaro <german.garcia.d@gmail.com>
2010-10-16 20:49:46 -0500

    Added class method refresh_top_musts to Must, in order to calculate the top musts

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101016202433) do
+ActiveRecord::Schema.define(:version => 20101017012939) do
 
   create_table "agrees", :force => true do |t|
     t.integer  "user_id"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(:version => 20101016202433) do
     t.integer  "must_id"
   end
 
+  create_table "followers", :force => true do |t|
+    t.integer  "following_id"
+    t.integer  "follower_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "follows", :force => true do |t|
     t.integer  "followable_id",                      :null => false
     t.string   "followable_type",                    :null => false
@@ -54,9 +61,11 @@ ActiveRecord::Schema.define(:version => 20101016202433) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
     t.string   "longitude"
     t.string   "latitude"
-    t.string   "url"
+    t.boolean  "top"
+    t.float    "top_value"
   end
 
   create_table "users", :force => true do |t|
