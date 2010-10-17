9ae079b16ac2ad6df1864002a3713cad019dfb0c
guilleiguaran <guilleiguaran@gmail.com>
2010-10-17 04:07:17 -0500

    Disabling refresh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   layout :get_layout
   
   def index
-    Must.refresh_top_musts
+    #Must.refresh_top_musts
     @musts = Must.find(:all, :conditions => ["top = ?", true], :order => "top_value DESC")
   end
   
