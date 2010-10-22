5cd5d63f4e0b94925a2119ba4f8aed46ef475139
Guillermo <guilleiguaran@gmail.com>
2010-10-21 23:37:00 -0500

    Testing new server configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,9 @@
-set :domain, "youmustdo.r10.railsrumble.com"
-set :application, "youmustdo"
+set :domain, "li74-76.members.linode.com"
+set :application, "youmustdo.com"
 set :deploy_to, "/var/www/apps/#{application}"
 set :user, "www-data"
 set :runner, user
 set :admin_runner, user
-set :app_server, :passenger
 set :rails_env, "production"
 set :keep_releases, 5
 
@@ -12,6 +11,10 @@ set :scm, :git
 set :branch, "master"
 set :repository, "git@github.com:railsrumble/rr10-team-167.git"
 set :deploy_via, :remote_cache
+
+set :unicorn_binary, "/usr/local/bin/unicorn"
+set :unicorn_config, "#{current_path}/config/unicorn/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 
 role :web, domain
@@ -23,10 +26,21 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+  task :start, :roles => :app, :except => { :no_release => true } do 
+      run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "#{try_sudo} kill `cat #{unicorn_pid}`"
+  end
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
+  end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+    stop
+    start
   end
   task :symlink_shared do
      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
